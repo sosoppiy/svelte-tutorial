0e94ac979a5f700e88282e50e9780dff02f97518
root <root@localhost.localdomain>
2020-11-19 18:23:31 +0900

    オブジェクト値の更新

--- src/reactive/UpdatingArraysAndbjects.svelte
+++ src/reactive/UpdatingArraysAndbjects.svelte
@@ -5,6 +5,8 @@
 	let array = [1,2,3,4,5,6,7,8,9,10];
 	let array2 = [1,2,3,4,5,6,7,8,9,10];
 
+	let obj = {id: 1, name: "hoge", age: 20};
+
 	function addNumberFaild() {
         //# ↓これだけだと、numbers配列の変化に応じて再描画してくれない。 ※配列自体は変わってるので、console.logで表示した時は値が追加されている
         numbers.push(numbers.length + 1);       
@@ -29,14 +31,22 @@
 		array2 = array2;
 	}
 
+	/**
+	 * オブジェクトの値更新
+	 */
+	function updateObjectValues() {
+		obj.age++;
+	}
+
 	$: sum = numbers.reduce((t, n) => t + n, 0);
 	$: sum2 = numbers2.reduce((t, n) => t + n, 0);
 	$: sum3 = numbers3.reduce((t, n) => t + n, 0);
 </script>
 
 <!-- チュートリアルの例 -->
-<div style="margin-bottom:30px">
+<div style="margin-bottom:100px">
 	<div>
+		<h3>チュートリアルの例</h3>
 		<p>{numbers.join(' + ')} = {sum}</p>
 	
 		<button on:click={addNumberFaild}>
@@ -63,14 +73,27 @@
 
 <!-- 自分で色々やってみる -->
 <div>
-	popも試してみる。
-	<div>
-		<p>{array.join(', ')}</p>
-		<button on:click={removeArrayFailed}>remove!再描画されない</button>
-	</div>
-	<div>
-		<p>{array2.join(', ')}</p>
-		<button on:click={removeArraySuccess}>remove!再描画される</button>
+	<h3>自分で色々試してみる</h3>
+
+	<div style="padding-bottom: 30px;">
+		<h4>popも試してみる。</h4>
+		<div>
+			<p>{array.join(', ')}</p>
+			<button on:click={removeArrayFailed}>remove!再描画されない</button>
+		</div>
+		<div>
+			<p>{array2.join(', ')}</p>
+			<button on:click={removeArraySuccess}>remove!再描画される</button>
+		</div>
 	</div>
 
+	<!-- オブジェクト -->
+	<div>
+		<h4>オブジェクトはどう？</h4>
+		<p>id: {obj.id}</p>
+		<p>name: {obj.name}</p>
+		<p>age: {obj.age}</p>
+
+		<button on:click={updateObjectValues}>誕生日を迎えたぜ</button>
+	</div>
 </div>
